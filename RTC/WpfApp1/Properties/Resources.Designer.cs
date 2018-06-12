660c96ae613a3aecc6ed4efbfd95dba67a4ac808
Einere <kjwsx23@gamil.com>
2018-06-12 23:05:53 +0900

    Add ignore

--- RTC/WpfApp1/Properties/Resources.Designer.cs
+++ RTC/WpfApp1/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace WpfApp1.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap save {
+            get {
+                object obj = ResourceManager.GetObject("save", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap send1 {
             get {
                 object obj = ResourceManager.GetObject("send1", resourceCulture);
